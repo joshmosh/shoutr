ef23fd46efdf04f2107ff5232a0e257caf3b9f13
Josh Minnich <joshminnich@gmail.com>
2018-05-09 14:37:42 -0500

    Extract Timeline and Dashboard classes
    Using single responsibility pricipal

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,8 +2,6 @@ class Shout < ApplicationRecord
   belongs_to :user
   belongs_to :content, polymorphic: true
 
-  default_scope { order(created_at: :desc) }
-
   delegate :username, to: :user
 
   validates :user, presence: true
