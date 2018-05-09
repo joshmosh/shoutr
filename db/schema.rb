eaa4ddbf257038e0fcd7dd62a5c146448792f6f7
Josh Minnich <joshminnich@gmail.com>
2018-05-09 10:12:53 -0500

    Add counter cache for follow count

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509031212) do
+ActiveRecord::Schema.define(version: 20180509144555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "following_relationships", force: :cascade do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_user_id"
+    t.integer  "follower_id",      null: false
+    t.integer  "followed_user_id", null: false
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.index ["follower_id", "followed_user_id"], name: "index_follower_following_relationship", unique: true, using: :btree
@@ -59,13 +59,15 @@ ActiveRecord::Schema.define(version: 20180509031212) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
-    t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "email",                                        null: false
+    t.string   "encrypted_password",   limit: 128,             null: false
+    t.string   "confirmation_token",   limit: 128
+    t.string   "remember_token",       limit: 128,             null: false
     t.string   "username"
+    t.integer  "followed_users_count",             default: 0, null: false
+    t.integer  "followers_count",                  default: 0, null: false
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
